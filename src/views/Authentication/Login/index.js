fa1c9546ab4d4e9620e8e0a8cf0af54ae4e857a3
NhatBao <mrbaocr@gmail.com>
2019-09-24 01:49:22 +0700

    Done Part2

--- src/views/Authentication/Login/index.js
+++ src/views/Authentication/Login/index.js
@@ -1,139 +1,235 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
-    StyleSheet, View, Text,
-    TouchableOpacity,
-    ImageBackground,
-    Keyboard
-} from "react-native";
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Keyboard,
+  StyleSheet,
+} from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import firebase from 'react-native-firebase'
+import AsyncStorage from '@react-native-community/async-storage'
 
-import {AsyncStorage} from "@react-native-community/async-storage";
-import { colors, storageKey } from "../../../utils";
-import { Input, Button } from "../../../components";
-import firebase, { Firebase } from "react-native-firebase";
+import { colors, storageKey } from '../../../utils';
+import { Input, Button, Modal } from '../../../components';
 
-const auth = firebase.auth();
+const auth = firebase.auth()
+
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       email: null,
+       password: null,
+       loginError: null,
+       isShowRegister: true,
+       isShowModal: false,
+    }
+  }
 
-    constructor(props) {
-        super(props)
+  componentDidMount() {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow.bind(this))
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyBoardDidHide.bind(this))
+  }
 
-        this.state = {
-            email: null,
-            password: null,
-            loginError: null    
+  componentWillUnmount() {
+    this.keyboardDidShowListener.remove()
+    this.keyboardDidHideListener.remove()
+  };
+  
+
+  _keyboardDidShow() {
+    this.setState({ isShowRegister: false })
+  }
+
+  _keyBoardDidHide() {
+    this.setState({ isShowRegister: true })
+  }
+  
+
+  _loginWithEmailPassword = async () => {
+    const { email, password } = this.state
+    try {
+      const user = await auth.signInWithEmailAndPassword(email, password)
+      if (user) {
+        const token = await auth.currentUser.getIdToken()
+        
+        if (token) {
+          await AsyncStorage.setItem(storageKey.token, JSON.stringify(token))
+          await AsyncStorage.setItem(storageKey.userInfo, JSON.stringify(user))
+          this.props.navigation.navigate('main')
         }
+      }
+    } catch (error) {
+      console.log('LOGIN ERROR', error.code)
+      this.setState({ loginError: error })
+      this._onRenderErrMess()
+      this._toggleModal()
     }
+  }
+  
+  _onRenderInputField() {
+    return (
+      <View style={styles.inputWrapper}>
+        <Input
+          placeholder='Email'
+          onChange={email => this.setState({ email })}
+        />
 
-    _loginWithEmailPassword = async () => {
-        const {email, password} = this.setState
-        try {
-            const user = auth.signInWithEmailAndPassword(email, password);
-            if (user) {
-                const token = await auth.currentUser.getIdToken()
-                if (token) {
-                    await AsyncStorage.setItem(storageKey.token, JSON.stringify(token))
-                    await AsyncStorage.setItem(storageKey.userInfo, JSON.stringify(user))
-                    this.props.navigation.navigate('main')
-                }
-            }
-        } catch (error) {
-            console.log('LOGIN ERR', error.code)
-            this.setState({loginErr: error})
-        }
+        <Input
+          placeholder='Password'
+          onChange={password => this.setState({ password })}
+          customStyle={{ marginTop: 50, marginBottom: 40 }}
+          secureTextEntry
+        />
+        <Button
+          onPress={() => this._loginWithEmailPassword()}
+        >Login</Button>
+      </View>
+    )
+  }
+
+  _onRenderForgetPassword() {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('reset')}
+        style={styles.forgetWrapper}
+      >
+        <Text style={styles.forgetTxt}>Quên mât khẩu?</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  // This is when user want to become a buddhist
+  _onRenderSuggestRegister() {
+    return (
+      <View style={styles.registerWrapper}>
+        <Text style={styles.registerTxt}>Chưa có tài khoản?</Text>
+
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate('register')}
+        >
+          <Text style={[styles.registerTxt, { fontWeight: '500' }]}>Đăng ký ngay.</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  } 
+
+  // Return error Message 
+
+  _onRenderErrMess = () => {
+    const { loginError } = this.state
+    if (loginError !== null) {
+      if (loginError.code === 'auth/invalid-email') {
+        return 'Email không đúng định dạng.'
+      } else if (loginError.code === 'auth/user-not-found') {
+        return 'Tài khoản không tồn tại.'
+      } else if (loginError.code === 'auth/wrong-password') {
+        return 'Sai mật khẩu.'
+      }
     }
+  }
 
+  // Toggle Modal
+  _toggleModal = () => {
+    this.setState({ isShowModal: !this.state.isShowModal })
+  }
 
-    render() {
-        return (
-            <ImageBackground
-                source={require('../../../assets/images/login-bg.jpg')}
-                style={styles.bgStyle}
-            >
-                <View style={styles.loginContainer}>
-                    <View style={styles.loginWrapper}>
-                        <Text style={styles.loginLogo}>
-                            BUDDHA
-                        </Text>
-                    </View>
-                    <View style={styles.inputWrapper}>
-                        <Input
-                            placeholder='Email'
-                            onChange={email => this.setState({ email })}
-                        />
-                        <Input
-                            placeholder='Password'
-                            onChange={password => this.setState({ password })}
-                            customStyle={{ marginTop: 10, marginBottom: 40 }}
-                            secureTextEntry
-                        />
-                        <Button
-                            onPress={() => this._loginWithEmailPassword}
-                        >Login</Button>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Reset')}>
-                            <Text style={styles.forgetTxt}
-                            >Quên mật khẩu ?</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')}>
-                            <Text style={styles.registerTxt}>
-                                Chưa có tài khoản? Đăng ký ngay.
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </ImageBackground>
-        );
+  // Render Modal
+  _onRenderModal = () => {
+    const { isShowModal } = this.state
+    console.log('HEY IS SHOW MODAL', isShowModal)
+    if (isShowModal) {
+      return (
+        <Modal
+          isVisible={isShowModal}
+          message={this._onRenderErrMess()}
+          onPress={() => this._toggleModal()}
+        />
+      )
     }
+  }
+
+  render() {
+    const { isShowRegister, isShowModal } = this.state
+    return (
+      <ImageBackground
+        source={require('../../../assets/images/login-bg.jpg')}
+        style={styles.backgroundImg}
+      >
+      <View style={styles.loginContainer}>
+        
+      <KeyboardAwareScrollView
+        style={{ width: '100%' }}
+        enableOnAndroid
+        extraHeight={100}
+        innerRef={ref => this.scroll = ref}
+      >
+          {/* App Name */}
+        <View style={styles.loginWrapper}>
+          <Text style={styles.appName}>BUDDHA</Text>
+          {/* Input Field */}
+         {this._onRenderInputField()}
+        {this._onRenderForgetPassword()}
+        </View>
+      </KeyboardAwareScrollView>
+       
+
+        {/* Need Register? */}
+        {isShowRegister ? this._onRenderSuggestRegister() : null}
+      </View>
+        {this._onRenderModal()}
+      </ImageBackground>
+    )
+  }
 }
 
-styles = StyleSheet.create({
-    bgStyle: {
-        flex: 1
-    },
-    loginContainer: {
-        flex: 1,
-        backgroundColor: colors.darkTransPrimary,
-        alignItems: "center",
-        paddingTop: 50
-    },
-    loginWrapper: {
-        width: "100%",
-        alignItems: "center",
-        paddingTop: 30
-    },
-    inputWrapper: {
-        width: "85%",
-        marginTop: 50
-    },
-    loginLogo: {
-        fontSize: 60,
-        fontWeight: "500",
-        color: colors.blueSecondary,
-        textTransform: "uppercase",
-        letterSpacing: 10
-    },
-    forgetWrapper: {
-        width: '65%',
-        marginTop: 10,
-    },
-    forgetTxt: {
-        color: colors.whitePrimary,
-        textAlign: 'right',
-        fontSize: 15,
-        marginTop: 15
-    },
-    registerWrapper: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginBottom: 15
-    },
-    registerTxt: {
-        color: colors.whitePrimary,
-        marginRight: 10,
-        alignSelf: "center",
-        marginTop: 30
-    }
+const styles = StyleSheet.create({
+  backgroundImg: {
+    flex: 1,
+  },
+  loginContainer: {
+    flex: 1,
+    backgroundColor: colors.darkTransPrimary,
+    paddingTop: 80,
+    alignItems: 'center',
+  },
+  loginWrapper: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  appName: {
+    color: colors.blueSecondary,
+    textTransform: 'uppercase',
+    fontSize: 40,
+    fontWeight: '500',
+    letterSpacing: 8,
+  },
+  inputWrapper: {
+    width: '85%',
+    marginTop: 100,
+  },
+  forgetWrapper: {
+    width: '65%',
+    marginTop: 10,
+  },
+  forgetTxt: {
+    color: colors.whitePrimary,
+    textAlign: 'right',
+    fontSize: 15,
+  },
+  registerWrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 15
+  },
+  registerTxt: {
+    color: colors.whitePrimary,
+    marginRight: 10
+  }
 })
